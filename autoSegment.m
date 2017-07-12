480641dac79650c5262e5a55895dee52264e01b7
xxning <xiaoning@mail.ustc.edu.cn>
2017-07-12 13:03:17 +0800

    optimizing

--- autoSegment.m
+++ autoSegment.m
@@ -20,19 +20,31 @@ for i=1e6:0.1*sample:l-2*ws
     end
 end
 
+l=length(seg);
+window=1*sample;
+remove=[];
+for i=1:l
+    if length(find((seg>=seg(i)-window)&(seg<=seg(i)+window)))<2
+        remove=[remove,i];
+    end
+end
+length(seg)
+seg(remove)=[];
+length(seg)
+
 Max=max(data);
 figure;
 subplot(211);
-plot(data);
+plot((0:length(data)-1)/sample,data);
 hold on;
 for i=1:length(seg)
-    plot([seg(i) seg(i)],[0 Max]);
+    plot([seg(i)/sample seg(i)/sample],[0 Max]);
 end
 
 diff2=diff(seg);
 seg1=[];
 Max1=max(diff2);
-threhold=0.5;
+threhold=0.35;
 for i=2:length(diff2)-1
     if diff2(i)>threhold*Max1 && diff2(i)>diff2(i+1)
         seg1=[seg1;i];
@@ -41,21 +53,21 @@ end
 
 seg2=[];
 for i=1:length(seg1)
-    seg2=[seg2;seg(seg1(i))+0.4e5;seg(seg1(i)+1)-1e5];
+    seg2=[seg2;seg(seg1(i))+0.6e5;seg(seg1(i)+1)-1e5];
 end
 
 seg2=[seg(1)-0.6e5;seg2;seg(end)];
 subplot(212);
-plot(data);
+plot((0:length(data)-1)/sample,data);
 hold on;
 for i=1:length(seg2)
-    plot([seg2(i) seg2(i)],[0 Max]);
+    plot([seg2(i)/sample seg2(i)/sample],[0 Max]);
 end
 
 segment={};
 
-if length(seg2)==20
-    for i=1:10
+if mod(length(seg2),2)==0
+    for i=1:length(seg2)/2
         segment{i}=data(seg2(2*i-1):seg2(2*i));
     end
 end
